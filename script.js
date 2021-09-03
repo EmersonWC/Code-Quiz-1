eb2ad144b38186dc4d8b5ed5e0407fcc385412b4
EmersonWC <emersonarevalo98@gmail.com>
2021-09-03 00:49:19 -0700

    <Start java>

---
+++ script.js
@@ -0,0 +1,26 @@
+// HTML Elements Being Called
+var quizBody = document.getElementById("Quiz")
+var quizTimer = document.getElementById("Timer")
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+var highScoreContainer = document.getElementById("highScoreContainer");
+var highScoreDiv = document.getElementById("highScore-Page"); 
+var highScoreDisplayName = document.getElementById("highScore-Initials");
+var highScoreDisplayScore = ("highScore-Score");
+var highScoreInputName = ("initials");
+var startQuizDiv = document.getElementById("mainPage");
+var endGameBtn = document.getElementById("endGameBtn");
+var startQuizButton = document.getElementById("mainBtn");
+var GameOverDiv = document.getElementById("GameOver");
+var submitScoreBtn = document.getElementById("submitScore");
+var resultsEl = document.getElementById("Q");
+var finalScoreEl = document.getElementById("finalScore");
+
+
+
+
+
+
+
