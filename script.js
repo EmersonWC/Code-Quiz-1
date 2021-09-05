2a69cca87ebd6af4a7defef6de4e8abf97ca05f1
EmersonWC <emersonarevalo98@gmail.com>
2021-09-04 22:24:24 -0700

    <Created function for clearing and generating highscores from localstorage>

--- script.js
+++ script.js
@@ -132,6 +132,23 @@ submitScoreBtn.addEventListener("click", function highscore(){
     }
 });
 
+// This function clears the list for the high scores and generates a new high score list from local storage
+function generateHighscores(){
+    highScoreDisplayName.innerHTML = "";
+    highScoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highScoreDisplayName.appendChild(newNameSpan);
+        highScoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+//
+
 
 
 
